2f145839f18d8ea44ff557090c74393c36033b3b
Ash McKenzie <ash@greenworm.com.au>
2012-11-23 00:02:32 +1100

    one more for the road

--- lib/ruby_tapas_proxy/feed.rb
+++ lib/ruby_tapas_proxy/feed.rb
@@ -5,7 +5,7 @@ module RubyTapasProxy
       # regex = Regexp.new('"(' + RubyTapasProxy::RUBY_TAPAS_DOWNLOAD_BASE_URL + '/[^"]+)" length="(\d+)"')
       # raw.gsub(regex, '"' + "#{site_url}/download?api_key=#{api_key}&amp;url=" + '\1' + "&amp;length=" + '\2" length="\2"')
       regex = Regexp.new('"(' + RubyTapasProxy::RUBY_TAPAS_DOWNLOAD_BASE_URL + '/[^"]+)"')
-      raw.gsub(regex, '"' + "#{site_url}/download?api_key=#{api_key}&amp;url=" + '\1')
+      raw.gsub(regex, '"' + "#{site_url}/download?api_key=#{api_key}&amp;url=" + '\1"')
     end
 
     def self.site_url_from_request request
